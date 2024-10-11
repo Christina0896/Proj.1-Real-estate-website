0bbd69904b60303f383e126baf324f4f50c7a020
Cristina <cristinabandeira82@gmail.com>
2024-10-11 01:31:01 +0100

    search bar

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const navbar = document.querySelector(".nav-links");
 hamburgerMenu.addEventListener("click", () => {
   navbar.classList.toggle("open");
 });
+
+//tab
+const tabMenu = document.querySelectorAll(".tab");
+tabMenu.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // Remove the 'active' class from all tabs
+    tabMenu.forEach((t) => {
+      t.classList.remove("active");
+    });
+    // Add the 'active' class to the clicked tab
+    tab.classList.toggle("active");
+  });
+});
