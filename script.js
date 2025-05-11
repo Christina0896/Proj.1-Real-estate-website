a67f2a1928b8a962643c30c97710a7ba5fda7eac
Cristina <cristinabandeira82@gmail.com>
2025-05-11 13:30:27 +0100

    fix: update navigation links to point to the correct home page

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 //icon menu for small screens
-const iconMenu = document.querySelector(".icon-menu");
-const navLink = document.querySelector(".nav-links");
 
-iconMenu.addEventListener("click", () => {
-  navLink.classList.toggle("active");
-});
 //tab options
-const tabs = document.querySelectorAll(".tab");
+const tabs = document.querySelectorAll('.tab');
 tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+  tab.addEventListener('click', () => {
     tabs.forEach((tab) => {
-      tab.classList.remove("active");
+      tab.classList.remove('active');
     });
-    tab.classList.add("active");
+    tab.classList.add('active');
   });
 });
 //advanced filters curtain
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.querySelector(".curtain-menu").style.display = "block";
+  document.querySelector('.curtain-menu').style.display = 'block';
 }
 function closeNav() {
-  document.querySelector(".curtain-menu").style.display = "none";
+  document.querySelector('.curtain-menu').style.display = 'none';
 }
